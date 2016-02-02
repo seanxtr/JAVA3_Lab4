6c364053345c9d5fc389b5165f72a423f4184ea2
Sheila Xu <Sheila Xu@s>
2016-02-02 11:07:34 -0800

    Project complete

--- JAVA_Lab4/src/FHlazySearchTree.java
+++ JAVA_Lab4/src/FHlazySearchTree.java
@@ -1,10 +1,8 @@
 import cs_1c.*;
-
 import java.util.*;
 
 public class FHlazySearchTree<E extends Comparable< ? super E > >
-   implements Cloneable
-{
+   implements Cloneable {
    protected int mSize; // number of undeleted nodes
    protected int mSizeHard; // number of all nodes
    protected FHlazySTNode<E> mRoot;
@@ -16,38 +14,70 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
    public void clear() { mSize = 0; mRoot = null; }
    public int showHeight() { return findHeight(mRoot, -1); }
 
-   
-   public E findMin() 
-   {
+   /**
+    * Method to find minimum value in the tree
+    * The method will ignore soft deleted nodes
+    * @return      minimum value
+    */
+   public E findMin() {
       if (mRoot == null)
          throw new NoSuchElementException();
-      return findMin(mRoot).data;
+      
+      FHlazySTNode<E> temp = findMinSkipDeleted(mRoot);
+      
+      // throw exception when nothing is found
+      if (temp == null)
+         throw new NoSuchElementException();
+      
+      return temp.data;
    }
    
-   public E findMax() 
-   {
+   /**
+    * Method to find maximum value in the tree
+    * The method will ignore soft deleted nodes
+    * @return      maximum value
+    */
+   public E findMax() {
       if (mRoot == null)
          throw new NoSuchElementException();
-      return findMax(mRoot).data;
+      
+      FHlazySTNode<E> temp = findMaxSkipDeleted(mRoot);
+      
+   // throw exception when nothing is found
+      if (temp == null)
+         throw new NoSuchElementException();
+      
+      return temp.data;
    }
    
-   public E find( E x )
-   {
+   /**
+    * Method to search a value
+    * @param x
+    * @return
+    */
+   public E find( E x ) {
       FHlazySTNode<E> resultNode;
       resultNode = find(mRoot, x);
       if (resultNode == null)
          throw new NoSuchElementException();
       return resultNode.data;
    }
-   public boolean contains(E x)  { return find(mRoot, x) != null; }
+   
+   /***
+    * Method to check if a value exists in the tree
+    * @param x
+    * @return
+    */
+   public boolean contains(E x)  { 
+      return find(mRoot, x) != null; 
+   }
    
    /**
     * Method to insert a new value to the tree
     * @param x     value to insert
     * @return      boolean indicates if the call success
     */
-   public boolean insert( E x )
-   {
+   public boolean insert( E x ) {
       int oldSize = mSize;
       mRoot = insert(mRoot, x);
       return (mSize != oldSize);
@@ -58,30 +88,27 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
     * @param x     value to remove
     * @return      boolean indicates if the call success
     */
-   public boolean remove( E x )
-   {
+   public boolean remove( E x ) {
       int oldSize = mSize;
       remove(mRoot, x);
       return (mSize != oldSize);
    }
    
    public < F extends Traverser<? super E > > 
-   void traverse(F func)
-   {
+   void traverse(F func) {
       traverse(func, mRoot);
    }
    
    /**
-    * 
+    * Method to make hard-copy of the current tree
     */
-   public Object clone() throws CloneNotSupportedException
-   {
+   public Object clone() throws CloneNotSupportedException {
 	  FHlazySearchTree<E> newObject = (FHlazySearchTree<E>)super.clone();
       newObject.clear();  // can't point to other's data
 
       newObject.mRoot = cloneSubtree(mRoot);
       newObject.mSize = mSize;
-      
+      newObject.mSizeHard = mSizeHard;
       return newObject;
    }
    
@@ -93,8 +120,12 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
    }
    
    // private helper methods ----------------------------------------
-   protected FHlazySTNode<E> findMin( FHlazySTNode<E> root ) 
-   {
+   /**
+    * Helper method to find the node with minimum value
+    * @param root           starting node
+    * @return               target node
+    */
+   protected FHlazySTNode<E> findMin( FHlazySTNode<E> root ) {
       if (root == null)
          return null;
       if (root.lftChild == null)
@@ -102,13 +133,46 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
       return findMin(root.lftChild);
    }
    
-   protected FHlazySTNode<E> findMax( FHlazySTNode<E> root ) 
-   {
+   /**
+    * Helper method to find the node with minimum value and exclude 
+    * the soft deleted nodes
+    * @param root           starting node
+    * @return               target node
+    */
+   protected FHlazySTNode<E> findMinSkipDeleted(FHlazySTNode<E> root) {
       if (root == null)
          return null;
-      if (root.rtChild == null)
+      
+      FHlazySTNode<E> temp = findMinSkipDeleted(root.lftChild);
+      
+      if (temp != null)
+         return temp;
+      
+      if (!root.deleted)
          return root;
-      return findMax(root.rtChild);
+      
+      return findMinSkipDeleted(root.rtChild);
+   }
+   
+   /**
+    * Helper method to find the node with maximum value excluding
+    * the soft deleted nodes
+    * @param root           starting node
+    * @return               target node
+    */
+   protected FHlazySTNode<E> findMaxSkipDeleted( FHlazySTNode<E> root ) {
+      if (root == null)
+         return null;
+      
+      FHlazySTNode<E> temp = findMaxSkipDeleted(root.rtChild);
+      
+      if (temp != null)
+         return temp;
+      
+      if (!root.deleted)
+         return root;
+      
+      return findMaxSkipDeleted(root.lftChild);
    }
    
    /**
@@ -117,13 +181,11 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
     * @param x        the value to insert
     * @return         the root node
     */
-   protected FHlazySTNode<E> insert( FHlazySTNode<E> root, E x )
-   {
+   protected FHlazySTNode<E> insert( FHlazySTNode<E> root, E x ) {
       int compareResult;  // avoid multiple calls to compareTo()
       
       // when the node to start is null, create a new one
-      if (root == null)
-      {
+      if (root == null) {
          mSize++;
          mSizeHard++;
          return new FHlazySTNode<E>(x, null, null);
@@ -135,7 +197,7 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
          root.lftChild = insert(root.lftChild, x);
       else if ( compareResult > 0 )
          root.rtChild = insert(root.rtChild, x);
-      else { 
+      else if (root.deleted) {
          // found the node and bring it back to live
          root.deleted = false;
          mSize++;
@@ -148,8 +210,7 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
     * @param root   the node to search
     * @param x      the target value to delete
     */
-   protected void remove(FHlazySTNode<E> root, E x )
-   {
+   protected void remove(FHlazySTNode<E> root, E x ) {
       int compareResult;
      
       // terminate recursion until root is null
@@ -163,7 +224,7 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
       else if ( compareResult > 0 )
          remove(root.rtChild, x);
       // found the node and set its deleted property to true
-      else {
+      else if (!root.deleted) {
           root.deleted = true;
           this.mSize--;
       }
@@ -172,12 +233,11 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
    
    /**
     * Helper method to perform hard deletion
-    * @param root
-    * @param x
-    * @return
+    * @param root        starting node
+    * @param x           target value
+    * @return            result node
     */
-   protected FHlazySTNode<E> removeHard(FHlazySTNode<E> root, E x)
-   {
+   protected FHlazySTNode<E> removeHard(FHlazySTNode<E> root, E x) {
       int compareResult;  // avoid multiple calls to compareTo()
      
       if (root == null)
@@ -188,25 +248,21 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
          root.lftChild = removeHard(root.lftChild, x);
       else if ( compareResult > 0 )
          root.rtChild = removeHard(root.rtChild, x);
-
       // found the node
-      else if (root.lftChild != null && root.rtChild != null)
-      {
-         root.data = findMin(root.rtChild).data;
+      else if (root.lftChild != null && root.rtChild != null) {
+         FHlazySTNode<E> temp = findMin(root.rtChild);
+         root.data = temp.data;
+         root.deleted = temp.deleted;
          root.rtChild = removeHard(root.rtChild, root.data);
-      }
-      else
-      {
+      } else {
          root = (root.lftChild != null)? root.lftChild : root.rtChild;
-         mSize--;
          mSizeHard--;
       }
       return root;
    }
    
    protected <F extends Traverser<? super E>> 
-   void traverse(F func, FHlazySTNode<E> treeNode)
-   {
+   void traverse(F func, FHlazySTNode<E> treeNode) {
       if (treeNode == null)
          return;
 
@@ -219,8 +275,7 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
       traverse(func, treeNode.rtChild);
    }
    
-   protected FHlazySTNode<E> find( FHlazySTNode<E> root, E x )
-   {
+   protected FHlazySTNode<E> find( FHlazySTNode<E> root, E x ) {
       int compareResult;  // avoid multiple calls to compareTo()
 
       if (root == null)
@@ -234,24 +289,31 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
       return root;   // found
    }
    
-   protected FHlazySTNode<E> cloneSubtree(FHlazySTNode<E> root)
-   {
+   /**
+    * Helper method to clone the input node and its child nodes
+    * @param root       starting node
+    * @return           result node
+    */
+   protected FHlazySTNode<E> cloneSubtree(FHlazySTNode<E> root) {
       FHlazySTNode<E> newNode;
+      
+      // end the method call when starting node is null
       if (root == null)
          return null;
 
       // does not set myRoot which must be done by caller
-      newNode = new FHlazySTNode<E>
-      (
+      newNode = new FHlazySTNode<E> (
          root.data, 
          cloneSubtree(root.lftChild), 
          cloneSubtree(root.rtChild)
       );
+      
+      // also clone the deletion state
+      newNode.deleted = root.deleted;
       return newNode;
    }
    
-   protected int findHeight( FHlazySTNode<E> treeNode, int height ) 
-   {
+   protected int findHeight( FHlazySTNode<E> treeNode, int height ) {
       int leftHeight, rightHeight;
       if (treeNode == null)
          return height;
@@ -261,44 +323,64 @@ public class FHlazySearchTree<E extends Comparable< ? super E > >
       return (leftHeight > rightHeight)? leftHeight : rightHeight;
    }
    
-   protected FHlazySTNode<E> collectGarbage(FHlazySTNode<E> root){
+   /**
+    * Helper method to perform hard removal of deleted nodes
+    * @param node         starting node
+    * @return             result node
+    */
+   protected FHlazySTNode<E> collectGarbage(FHlazySTNode<E> node){
       // terminate the recursion when reaching the end of tree
-      if (root == null)
+      if (node == null)
          return null;
 
-      // check if the root should be deleted
-      if (root.deleted)
-         mRoot = removeHard(root, root.data);
+      // check if current node should be deleted
+      if (node.deleted) {
+         // perform hard deletion on current node
+         node = removeHard(node, node.data);
+         
+         // recursive call to continue the progress
+         collectGarbage(node);
+      } else {
+         // when current node is active, do collect garbage on its child-nodes
+         collectGarbage(node.rtChild);
+         collectGarbage(node.lftChild);
+      }
       
-      collectGarbage(root.rtChild);
-      return root;   
+      return node;
    }
 }
 
 /**
- * 
+ * Class of binary tree node to store actual data value and used as
+ * the basic building block of the binary search tree
  * @author Tianrong Xiao
  *
- * @param <E>
+ * @param <E>     Generic data type stored by the class
  */
-class FHlazySTNode<E extends Comparable< ? super E > >
-{
+class FHlazySTNode<E extends Comparable< ? super E > > {
    // use public access so the tree or other classes can access members
    public boolean deleted;
    public FHlazySTNode<E> lftChild, rtChild;
    public E data;
    public FHlazySTNode<E> myRoot;  // needed to test for certain error
 
-   public FHlazySTNode( E d, FHlazySTNode<E> lft, FHlazySTNode<E> rt )
-   {
+   /**
+    * Three parameters constructor
+    * @param d          value to store
+    * @param lft        left child node
+    * @param rt         right child node
+    */
+   public FHlazySTNode( E d, FHlazySTNode<E> lft, FHlazySTNode<E> rt ) {
       deleted = false;
       lftChild = lft; 
       rtChild = rt;
       data = d;
    }
    
-   public FHlazySTNode()
-   {
+   /**
+    * Default constructor
+    */
+   public FHlazySTNode() {
       this(null, null, null);
    }
    
--- JAVA_Lab4/src/Foothill.java
+++ JAVA_Lab4/src/Foothill.java
@@ -1,28 +1,28 @@
 // CIS 1C Assignment #4
 // Part A - Lazy Deletion
 
+import java.util.*;
 import cs_1c.*;
 
-class PrintObject<E> implements Traverser<E>
-{
-   public void visit(E x)
-   {
+class PrintObject<E> implements Traverser<E> {
+   public void visit(E x) {
       System.out.print( x + " ");
    }
 };
 
 //------------------------------------------------------
-public class Foothill
-{
+public class Foothill {
    // -------  main --------------
-   public static void main(String[] args) throws Exception
-   {
+   public static void main(String[] args) throws Exception {
       int k;
       FHlazySearchTree<Integer> searchTree = new FHlazySearchTree<Integer>();
       PrintObject<Integer> intPrinter = new PrintObject<Integer>();
 
+      System.out.println("test on a hard-empty tree");
       searchTree.traverse(intPrinter);
-
+      TestTreeMinMax(searchTree);
+      
+      // test insertion
       System.out.println( "\ninitial size: " + searchTree.size() );
       searchTree.insert(50);
       searchTree.insert(20);
@@ -35,12 +35,15 @@ public class Foothill
       searchTree.traverse(intPrinter);
       System.out.println( "\ntree 1 size: " + searchTree.size() 
          + "  Hard size: " + searchTree.sizeHard() );
-
+      
       System.out.println( "Collecting garbage on new tree - should be " +
             "no garbage." );
       searchTree.collectGarbage();
       System.out.println( "tree 1 size: " + searchTree.size() 
          + "  Hard size: " + searchTree.sizeHard() );
+      
+      System.out.print("test min and max on a hard-filled tree\n");
+      TestTreeMinMax(searchTree);
       
       // test assignment operator
       FHlazySearchTree<Integer> searchTree2 
@@ -91,11 +94,14 @@ public class Foothill
       System.out.println( "\n\nAttempting 100 removals: " );
       for (k = 100; k > 0; k--)
       {
-         if (searchTree.remove(k))
+         if (searchTree.remove(k)) {
             System.out.println( "removed " + k );
+         }
       }
+      System.out.println("test min and max on a soft-empty tree");
+      TestTreeMinMax(searchTree);
+      
       searchTree.collectGarbage();
-
       System.out.println( "\nsearch_tree now:");
       searchTree.traverse(intPrinter);
       System.out.println( "\ntree 1 size: " + searchTree.size() 
@@ -111,11 +117,38 @@ public class Foothill
       searchTree2.traverse(intPrinter);
       System.out.println( "\ntree 2 size: " + searchTree2.size() 
          + "  Hard size: " + searchTree2.sizeHard() );
+      
+      TestTreeMinMax(searchTree2);
+      
+      System.out.println( "\nRemove 10 from the tree 2:" );
+      searchTree2.remove(10);
+      TestTreeMinMax(searchTree2);
+      
+      System.out.println( "\nRemove 700 from the tree 2:" );
+      searchTree2.remove(700);
+      TestTreeMinMax(searchTree2);
+      
+      System.out.println( "\nRemove 200 from the tree 2:" );
+      searchTree2.remove(200);
+      TestTreeMinMax(searchTree2);
+   }
+   
+   private static void TestTreeMinMax(FHlazySearchTree<Integer> st) {
+      try {
+         System.out.printf("Min in tree: %d and Max in tree: %d%n", 
+            st.findMin(), st.findMax());
+      }
+      catch (NoSuchElementException e){
+    	  System.out.println("Error: Can't find min or max in the tree");
+      }
    }
 }
 
 
 /* ---------------------- Run --------------------------
+
+test on a hard-empty tree
+Error: Can't find min or max in the tree
 
 initial size: 0
 After populating -- traversal and sizes: 
@@ -123,6 +156,8 @@ After populating -- traversal and sizes:
 tree 1 size: 6  Hard size: 6
 Collecting garbage on new tree - should be no garbage.
 tree 1 size: 6  Hard size: 6
+test min and max on a hard-filled tree
+Min in tree: 10 and Max in tree: 70
 
 
 Attempting 1 removal: 
@@ -154,6 +189,8 @@ removed 50
 removed 30
 removed 22
 removed 10
+test min and max on a soft-empty tree
+Error: Can't find min or max in the tree
 
 search_tree now:
 
@@ -163,4 +200,15 @@ searchTree2:
 
 10 20 30 50 60 70 100 200 300 500 600 700 
 tree 2 size: 12  Hard size: 12
+Min in tree: 10 and Max in tree: 700
+
+Remove 10 from the tree 2:
+Min in tree: 20 and Max in tree: 700
+
+Remove 700 from the tree 2:
+Min in tree: 20 and Max in tree: 600
+
+Remove 200 from the tree 2:
+Min in tree: 20 and Max in tree: 600
+
 ---------------------------------------------------------------------- */
